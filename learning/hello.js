f71c22aa79ad4b33d92028e03cd8b4633ff3c5bd
asaadalbadi <8asaad4@gmail.com>
2024-07-14 17:37:03 -0300

    Initial commit

---
+++ learning/hello.js
@@ -0,0 +1,25 @@
+// function capitalized (theInput){
+//     let x = theInput;
+//     x.toUpp
+    
+// }
+// capitalized("Hello World");
+// function myFun(){
+//     document.getElementById("demo").innerHTML = "Content changedf"
+// }
+
+let fName = prompt("enter a number: ");
+for (let i=0; i<20; i++){
+  console.log(fName+" x "+i+" = "+(fName*i));
+}
+function haveFun(){
+  a =9;
+  var a = 8;
+
+// It store array data type
+let places= ["GFG", "Computer", "Hello"];
+    document.getElementById("rashid").innerHTML = a
+  };
+
+const btn = document.getElementById("btn");
+btn.addEventListener()
